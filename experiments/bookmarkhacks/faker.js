0bb0d93f08759b23557ebfad5623c5986c2d83c3
script-js <134544514+script-js@users.noreply.github.com>
2024-01-05 19:34:16 -0800

    Add files via upload

---
+++ experiments/bookmarkhacks/faker.js
@@ -0,0 +1,17 @@
+document.head = document.head || document.getElementsByTagName('head')[0]
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+setTimeout(function() {
+  document.title = "Reading To Do, i-Ready";
+  changeFavicon("https://login.i-ready.com/favicon.ico")
+}, 1000)
