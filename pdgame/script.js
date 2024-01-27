e0be270190df38751f55a24adf477e005419775a
script-js <134544514+script-js@users.noreply.github.com>
2024-01-27 15:12:46 -0800

    Update script.js

--- pdgame/script.js
+++ pdgame/script.js
@@ -34,7 +34,7 @@ function touches(ob1, ob2) {
   var rect2 = ob2.getBoundingClientRect();
   var btm = topp + 100;
   
-  if (rect1.right == rect2.left && btm > parseInt(slingshot.style.top) && topp < parseInt(slingshot.style.top)) {
+  if (rect1.right > rect2.left && btm > parseInt(slingshot.style.top) && topp < parseInt(slingshot.style.top)) {
      return true;
   } else {
     return false
